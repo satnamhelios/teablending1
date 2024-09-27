ac8f951c9af0f7cf3a13819c6e559c72d4c80ac3
GarvitDudeja <68330739+Garvitdudeja@users.noreply.github.com>
2024-09-27 13:13:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 // Use built-in middleware for JSON and URL-encoded data
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
+// app.use(cors());
 app.use(express.json({ extended: true }));
 
 app.use("/", routes);
